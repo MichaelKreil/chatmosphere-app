1b906149df8da4198fb537d8768799615983cd65
Michael Kreil <github@michael-kreil.de>
2021-03-08 18:44:18 +0100

    adding background

--- src/components/Room/Room.tsx
+++ src/components/Room/Room.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 import { panOptions } from '../PanWrapper/panOptions';
+import background from './../../assets/background.png';
 
 /* fixed size won't work, because when scale is 1 there will be room to pan; but the plugin won't allow it because scale is 1. 
 the fix is to set the size of the react-transform-component and react-transform-element exlusively (see App.css) */
 
 
 const RoomContainer = styled.div`
+  background-image:url("${background}");
+  background-repeat: no-repeat;
+  background-position: center center;
+  Background-size: contain;
+  image-rendering: pixelated;
   width:${panOptions.room.size.x}px;height:${panOptions.room.size.y}px;
   box-sizing: border-box;
   display:block;
