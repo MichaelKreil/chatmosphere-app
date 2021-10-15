a1a9c77b8bebb2c8c945e90e0177eb795643c4ec
Michael Kreil <github@michael-kreil.de>
2021-10-15 17:24:38 +0200

    update background

--- src/components/Room/Room.tsx
+++ src/components/Room/Room.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { panOptions } from '../PanWrapper/panOptions';
-import background from './../../assets/background.png';
+import background from './../../assets/background.svg';
 
 /* fixed size won't work, because when scale is 1 there will be room to pan; but the plugin won't allow it because scale is 1. 
 the fix is to set the size of the react-transform-component and react-transform-element exlusively (see App.css) */
